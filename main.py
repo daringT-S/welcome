39c72625e613c55812dbcf613a4f8011a11c72fe
Shionoya12 <shionoyakouta@gmail.com>
2026-04-16 20:06:06 +0900

    Add func_mtanaka to main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from func_mtanaka import func_mtanaka
 from func_yotsuki import func_yotsuki
+from func_kshionoya import func_kshionoya
 
 print("2026")
 func_mtanaka()
 func_yotsuki()
-
+func_kshionoya()
