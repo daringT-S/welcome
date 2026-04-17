794d08ce537b422c34c22de8c23dadd205954a93
hliu <liu.h.8454@m.isct.ac.jp>
2026-04-17 20:28:17 +0900

    Add func_hliu to main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from func_mtanaka import func_mtanaka
 from func_yotsuki import func_yotsuki
 from func_kshionoya import func_kshionoya
+from func_hliu import func_hliu
 
 print("2026")
 func_mtanaka()
 func_yotsuki()
 func_kshionoya()
+func_hliu()
