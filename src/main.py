171b33fc6ad02b0da284d297c61e01a25a3a9788
c-xacap <c.xacap@gmail.com>
2026-04-20 09:36:27 +0900

    Move func_ksergelen into src and wire into main.py

--- src/main.py
+++ src/main.py
@@ -2,9 +2,11 @@ from func_mtanaka import func_mtanaka
 from func_yotsuki import func_yotsuki
 from func_kshionoya import func_kshionoya
 from func_hliu import func_hliu
+from func_ksergelen import func_ksergelen
 
 print("2026")
 func_mtanaka()
 func_yotsuki()
 func_kshionoya()
 func_hliu()
+func_ksergelen()
